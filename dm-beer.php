bc68f085a91b4718171af7b9b200e4f8af3e9407
bradmkjr <brad@ucanbonline.net>
2014-07-23 10:54:47 -0600

    added in beer_categories

--- dm-beer.php
+++ dm-beer.php
@@ -3,7 +3,7 @@
 Plugin Name: DM Missoula Beer
 Plugin URI: http://draughtworksbrewery.com/
 Description: Helping ugly people get laid since 1862
-Version: 1.0.3
+Version: 1.0.5
 Author: Bradford Knowlton
 Author URI: http://bradknowlton.com/
 License: GPLv2
@@ -13,6 +13,38 @@ GitHub Branch:     master
 
 add_action( 'init', 'register_cpt_beer' );
 function register_cpt_beer() {
+	
+	$labels = array(
+		'name' => _x( 'Beer Categories', 'beer_categories' ),
+		'singular_name' => _x( 'Beer Category', 'beer_categories' ),
+		'search_items' => _x( 'Search Beer Categories', 'beer_categories' ),
+		'popular_items' => _x( 'Popular Beer Categories', 'beer_categories' ),
+		'all_items' => _x( 'All Beer Categories', 'beer_categories' ),
+		'parent_item' => _x( 'Parent Beer Category', 'beer_categories' ),
+		'parent_item_colon' => _x( 'Parent Beer Category:', 'beer_categories' ),
+		'edit_item' => _x( 'Edit Beer Category', 'beer_categories' ),
+		'update_item' => _x( 'Update Beer Category', 'beer_categories' ),
+		'add_new_item' => _x( 'Add New Beer Category', 'beer_categories' ),
+		'new_item_name' => _x( 'New Beer Category', 'beer_categories' ),
+		'separate_items_with_commas' => _x( 'Separate beer categories with commas', 'beer_categories' ),
+		'add_or_remove_items' => _x( 'Add or remove Beer Categories', 'beer_categories' ),
+		'choose_from_most_used' => _x( 'Choose from most used Beer Categories', 'beer_categories' ),
+		'menu_name' => _x( 'Beer Categories', 'beer_categories' ),
+	);
+	$args = array(
+		'labels' => $labels,
+		'public' => true,
+		'show_in_nav_menus' => true,
+		'show_ui' => true,
+		'show_tagcloud' => true,
+		'show_admin_column' => false,
+		'hierarchical' => true,
+		'rewrite' => true,
+		'query_var' => true
+	);
+	register_taxonomy( 'beer_categories', array('beer'), $args );
+
+	
 	$labels = array(
 		'name' => _x( 'Beers', 'beer' ),
 		'singular_name' => _x( 'Beer', 'beer' ),
